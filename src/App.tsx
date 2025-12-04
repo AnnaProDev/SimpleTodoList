d745d8efb1970e0d283a3cb39978e4d0f4142ae6
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2025-12-04 18:06:40 -0500

    Connect database via API and display fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
-
-const tasks = [
-  {
-    id: 1,
-    title: "Купить продукты на неделю",
-    isDone: false,
-    addedAt: "1 сентября",
-    priority: 2,
-  },
-  {
-    id: 2,
-    title: "Полить цветы",
-    isDone: true,
-    addedAt: "2 сентября",
-    priority: 0,
-  },
-  {
-    id: 3,
-    title: "Сходить на тренировку",
-    isDone: false,
-    addedAt: "3 сентября",
-    priority: 1,
-  },
-  {
-    id: 4,
-    title: "Срочно отправить рабочий отчет",
-    isDone: false,
-    addedAt: "4 сентября",
-    priority: 4,
-  },
-  {
-    id: 5,
-    title: "Заплатить за коммунальные услуги",
-    isDone: false,
-    addedAt: "3 сентября",
-    priority: 3,
-  },
-]
-
 
 
 function App() {
 
 	const [ selectedTaskId, setSelectedTaskId] = useState();
+	const [ tasks, setTasks] = useState(null)
 
 	function setPriorityColor(priority: number) {
 		
@@ -62,6 +24,18 @@ function App() {
 			return "#ffffff";
 		}
 	}
+
+	useEffect (() => {
+		fetch(import.meta.env.VITE_API_URL, {
+		headers: {
+			"api-key": import.meta.env.VITE_API_KEY,
+		},
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				setTasks(json.data)
+			})
+	}, [])
 
 	if (tasks == null) {
 		return (
@@ -82,13 +56,14 @@ function App() {
 			{tasks.map( (task) => (
 				<li className="task" 
 					key={task.id} 
-					style ={{ background : setPriorityColor(task.priority), borderColor: selectedTaskId === task.id ? "blue" : "" }}
+					style ={{ background : setPriorityColor(task.attributes.priority), borderColor: selectedTaskId === task.id ? "blue" : "" }}
 					onClick={() => (setSelectedTaskId(task.id))}
 					>
-					<input className='task_check' type="checkbox" checked={task.isDone} />
+					<input className='task_check' type="checkbox" checked={task.attributes.status} />
 					<div>
-						<div className='task_title' style ={{ textDecorationLine: task.isDone ? "line-through": "none" }}>{task.title}</div>
-						<div>Date of creation: {task.addedAt}</div>
+						<div className='task_title' style ={{ textDecorationLine: task.attributes.status ? "line-through": "none" }}>{task.attributes.title}</div>
+						<div> 
+							<b>Date of creation:</b>: {new Date(task.attributes.addedAt).toLocaleDateString()}</div>
 					</div>
 				</li>
 			))}
